3c57fddd96ff43cea87fcd9aaf42ce67ae24bc8e
Kevin Nord <knord@meredith.com>
2016-09-21 09:08:56 -0700

    Created nuspec and (un)install scripts

---
+++ Source/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName= 'Screenhero' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://secure.screenhero.com/update/win/Screenhero-Latest-setup.exe' # download url
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs   = '/S'           # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
+
+  # optional, highly recommended
+  softwareName  = 'Screenhero*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = ''
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = ''
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
